38830949367b5460f08803605e937e071900729e
shunchang <1966807489@qq.com>
2021-10-20 13:48:06 +0800

    update code

--- RobustART/noise/utils/add_noise_utils.py
+++ RobustART/noise/utils/add_noise_utils.py
@@ -1,10 +1,10 @@
 from PIL import Image
 from RobustART.noise.utils.imagenet_c import corrupt
-from RobustART.noise.utils.adv import pgd_l1, pgd_l2, pgd_linf, clip_l2_norm, autoattack_linf, mim_linf, fgsm
+from RobustART.noise.utils.adv import pgd_l1, pgd_l2, pgd_linf, clip_l2_norm, autoattack_linf, mim_linf, fgsm, cw2, deepfool, ead
 from .imagenet_s_gen import ImageTransfer
 
 
-noise_list = ['imagenet-s', 'imagenet-c', 'pgd_linf', 'pgd_l2', 'fgsm', 'autoattack_linf', 'mim_linf', 'pgd_l1']
+noise_list = ['imagenet-s', 'imagenet-c', 'pgd_linf', 'pgd_l2', 'fgsm', 'autoattack_linf', 'mim_linf', 'pgd_l1', 'cw2', 'deepfool', 'ead']
 
 default_config = {
     'imagenet-s': {'decoder_type': 'pil', 'resize_type': 'pil-bilinear', 'transform_type': 'val'},
@@ -15,6 +15,9 @@ default_config = {
     'autoattack_linf': {'model': None, 'norm': 'Linf', 'eps': 8/255, 'version': 'standard', 'verbose': False},
     'mim_linf': {'model': None, 'eps': 8/255, 'num_steps': 20, 'step_size': 0.002, 'decay_factor': 1.0},
     'pgd_l1': {'model': None, 'eps': 1600.0, 'input_size': 224, 'eps_step': 120, 'max_iter': 20, 'batch_size': 16}
+    'cw2': {'model': None, 'device': None, 'IsTarget': None, 'kappa': 0, 'lr': 0.2, 'init_const': 0.01, 'lower_bound': 0.0, 'upper_bound': 1.0, 'max_iter': 200, 'binary_search_steps': 4}
+    'deepfool': {'model': None, 'device': None, 'IsTarget': None, 'overshoot': 0.02, 'max_iter': 10}
+    'ead': {'model': None, 'device': None, 'IsTarget': None, 'kappa': 0, 'lr': 0.2, 'init_const': 0.02, 'lower_bound': 0.0, 'upper_bound': 1.0, 'max_iter': 50, 'binary_search_steps': 3, 'class_type_number': 1000, 'beta': 1e-3, 'EN': True}
 }
 
 
@@ -47,5 +50,8 @@ function_dict = {
     'fgsm': fgsm,
     'autoattack_linf': autoattack_linf,
     'mim_linf': mim_linf
+    'cw2': cw2
+    'deepfool': deepfool
+    'ead': ead
 }
 
